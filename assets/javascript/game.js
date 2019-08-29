803648b89219f96dcd1a0a3ea7f4e5539c6079b7
clarymt <clarymt@me.com>
2019-08-28 21:14:50 -0400

    made a lot of progress with javascript but the img click function needs debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,25 @@
+$( document ).ready(function() {
+
+
 var targetScore;
 var loss;
 var win;
 
-$("#result").html('hello world');
+
+targetScore = Math.floor(Math.random() * 129 ) + 30; 
+
+$("#result").text('Target Score: ' + targetScore);
+//console.log(targetScore);
+
 //this game will have 4 crystals and random target number
 for(var i = 0; i < 4; i++){
     
-    var random = Math.floor(Math.random() * 12);
-    console.log(random);
+    //each crystal will have random number 1-12
+ 
+    var random = Math.floor(Math.random() * 12) + 1;
+    //console.log(random);
+
+
     var crystal = $("<div>");
         crystal.attr({
             "class": 'crystal',
@@ -15,13 +27,19 @@ for(var i = 0; i < 4; i++){
         });
 
     $(".crystals").append(crystal);
-
-
 }
-//each crystal will have random number 1-12
+
+$(".crystal").on('click', function () {
+
+    var num = $(this).attr('data-random');
+    
+    console.log(typeof num);
+
+});
 
 //each new game will generate a new random bumber for each crystal
 //when clicking a crystal, it should adding to the total until
 //it equals target score
 //if it is greater than target number it recieves +1 loss and will restart
 //if it is equal, it recieves +1 win and will restart
+});
